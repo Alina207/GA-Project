9e9e5a36f8461e1d88c5c0867a7079180667d9c1
Alina207 <Alina@Alinas-MacBook-Pro-4.local>
2016-10-06 18:54:07 -0400

    meep

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(){
+
+ $(window).scroll(function() {
+    if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/ 
+    {
+        $('.opaque-navbar').addClass('opaque');
+    } else {
+        $('.opaque-navbar').removeClass('opaque');
+    }
+});
+    
+});
